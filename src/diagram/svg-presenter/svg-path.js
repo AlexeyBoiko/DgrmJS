f7ff33b88368450f6256136ffe8acec9311f3872
Alexey Boyko <alexeybbb@gmail.com>
2022-01-10 20:52:38 +0300

    SvgPath

--- src/diagram/svg-presenter/svg-path.js
+++ src/diagram/svg-presenter/svg-path.js
@@ -2,7 +2,7 @@
 export class SvgPath {
 	/**
 	 * @param {object} param
-	 * @param {SVGGraphicsElement} param.svgEl
+	 * @param {SVGPathElement} param.svgEl
 	 * @param {PresenterPathEnd} param.start
 	 * @param {PresenterPathEnd} param.end
 	 */
@@ -22,31 +22,24 @@ export class SvgPath {
 	 * @returns {void}
 	 */
 	update(endType, param) {
-		const coef = 70;
-		if (this.point1.shapePosition.shape === shapePosition.shape) {
-			// const x = this._start.position;
-			const y = this.point1.innerPosition.y + shapePosition.position.y;
-			/** @private */
-			this._p1Part = `M ${x} ${y} C ${SvgPath._cx(this.point1.dir, x, coef)} ${SvgPath._cy(this.point1.dir, y, coef)}, `;
+		if (endType === 'start') {
+			this._start = param;
+		} else {
+			this._end = param;
 		}
-
-		if (this.point2.shapePosition.shape === shapePosition.shape) {
-			const x = this.point2.innerPosition.x + shapePosition.position.x;
-			const y = this.point2.innerPosition.y + shapePosition.position.y;
-			/** @private */
-			this._p2Part = `${SvgPath._cx(this.point2.dir, x, coef)} ${SvgPath._cy(this.point2.dir, y, coef)}, ${x} ${y}`;
-		}
-
-		return `${this._p1Part}${this._p2Part}`;
+		this._svgEl.setAttribute('d', SvgPath._calcDAttr(70, this._start, this._end));
 	}
 
 	/**
 	 * @param {number} coef
 	 * @param {PresenterPathEnd} start
 	 * @param {PresenterPathEnd} end
+	 * @returns {string}
+	 * @private
 	 */
 	static _calcDAttr(coef, start, end) {
-		`M ${start.position.x} ${start.position.y} C ${SvgPath._cx(start.dir, start.position.x, coef)} ${SvgPath._cy(start.dir, start.position.y, coef)}, `
+		return `M ${start.position.x} ${start.position.y} C ${SvgPath._cx(start.dir, start.position.x, coef)} ${SvgPath._cy(start.dir, start.position.y, coef)}, ` +
+			`${SvgPath._cx(end.dir, end.position.x, coef)} ${SvgPath._cy(end.dir, end.position.y, coef)}, ${end.position.x} ${end.position.y}`;
 	}
 
 	/**
