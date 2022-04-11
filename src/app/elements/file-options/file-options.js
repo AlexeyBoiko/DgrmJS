5a0beee1bafad6cbcb594bad1272611ded3bb655
Alexey Boyko <alexeybbb@gmail.com>
2022-04-11 18:19:58 +0300

    donate link

--- src/app/elements/file-options/file-options.js
+++ src/app/elements/file-options/file-options.js
@@ -21,14 +21,15 @@ export class FileOptions extends HTMLElement {
 				left: 0px;
 			}
 
-			.options [data-cmd] { 
+			.options div, .options a { 
 				color: rgb(13, 110, 253); 
 				cursor: pointer; margin: 10px 0;
 				display: flex;
 				align-items: center;
 				line-height: 25px;
+				text-decoration: none;
 			}
-			.options [data-cmd] svg { margin-right: 10px; }
+			.options div svg, .options a svg { margin-right: 10px; }
 			</style>
 			 <svg class="menu" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M3 4h18v2H3V4zm0 7h18v2H3v-2zm0 7h18v2H3v-2z" fill="rgba(52,71,103,1)"/></svg>
 			 <div class="options" style="visibility: hidden;">
@@ -37,6 +38,9 @@ export class FileOptions extends HTMLElement {
 				<div data-cmd="dgrmOpen"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M3 21a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h7.414l2 2H20a1 1 0 0 1 1 1v3h-2V7h-7.414l-2-2H4v11.998L5.5 11h17l-2.31 9.243a1 1 0 0 1-.97.757H3zm16.938-8H7.062l-1.5 6h12.876l1.5-6z" fill="rgba(52,71,103,1)"/></svg>Open diagram image</div>
 				<div data-cmd="dgrmSave"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M3 19h18v2H3v-2zm10-5.828L19.071 7.1l1.414 1.414L12 17 3.515 8.515 4.929 7.1 11 13.17V2h2v11.172z" fill="rgba(52,71,103,1)"/></svg>Save diagram image</div>
 				<div data-cmd="dgrmGenerateLink"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M13.06 8.11l1.415 1.415a7 7 0 0 1 0 9.9l-.354.353a7 7 0 0 1-9.9-9.9l1.415 1.415a5 5 0 1 0 7.071 7.071l.354-.354a5 5 0 0 0 0-7.07l-1.415-1.415 1.415-1.414zm6.718 6.011l-1.414-1.414a5 5 0 1 0-7.071-7.071l-.354.354a5 5 0 0 0 0 7.07l1.415 1.415-1.415 1.414-1.414-1.414a7 7 0 0 1 0-9.9l.354-.353a7 7 0 0 1 9.9 9.9z" fill="rgba(52,71,103,1)"/></svg>Copy link to diagram</div>
+				<a href="https://www.patreon.com/dgrm" target="_blank">
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0H24V24H0z"/><path d="M12.001 4.529c2.349-2.109 5.979-2.039 8.242.228 2.262 2.268 2.34 5.88.236 8.236l-8.48 8.492-8.478-8.492c-2.104-2.356-2.025-5.974.236-8.236 2.265-2.264 5.888-2.34 8.244-.228zm6.826 1.641c-1.5-1.502-3.92-1.563-5.49-.153l-1.335 1.198-1.336-1.197c-1.575-1.412-3.99-1.35-5.494.154-1.49 1.49-1.565 3.875-.192 5.451L12 18.654l7.02-7.03c1.374-1.577 1.299-3.959-.193-5.454z" fill="rgba(255,66,77,1)"/></svg>Donate
+				</a>
 		 	</div>`;
 		shadow.querySelectorAll('svg').forEach(el => el.addEventListener('click', this));
 		shadow.querySelectorAll('[data-cmd]').forEach(el => el.addEventListener('click', this));
