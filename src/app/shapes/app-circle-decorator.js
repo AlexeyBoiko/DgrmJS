38b618962abce7d64a25d17a2ae1b807a3093ea9
Alexey Boyko <alexeybbb@gmail.com>
2022-05-06 22:22:08 +0300

    resize circle - tmp

--- src/app/shapes/app-circle-decorator.js
+++ src/app/shapes/app-circle-decorator.js
@@ -6,18 +6,45 @@ export class AppCircleDecorator extends SvgShapeTextEditorDecorator {
 	 * @param {SVGTextElement} textEl
 	 */
 	onTextChange(textEl) {
-		if (!this._circle) {
-			/**
-			 * @type {SVGCircleElement}
-			 * @private
-			 */
-			this._circle = this.svgEl.querySelector('[data-key="main"]');
-		}
-
-		if (svgTextIsOut(textEl, this._circle, 5)) {
-			this._circle.r.baseVal.value = this._circle.r.baseVal.value * 1.25;
-		}
-
 		super.onTextChange(textEl);
+
+		if (!this._circle) {
+			/** @private */
+			this._circle = /** @type {SVGCircleElement}} */ (this.svgEl.querySelector('[data-key="main"]'));
+
+			/** @private */
+			this._minR = this._circle.r.baseVal.value;
+
+			this._copy = /** @type {SVGCircleElement}} */ (this._circle.cloneNode(false));
+			this._copy.style.fill = 'transparent';
+			this._copy.style.stroke = 'transparent';
+			this.svgEl.appendChild(this._copy);
+		}
+
+		if (this._isOut(textEl)) {
+			do { this._copy.r.baseVal.value = this._copy.r.baseVal.value + 20; }
+			while (this._isOut(textEl));
+
+			this._circle.r.baseVal.value = this._copy.r.baseVal.value;
+		} else {
+			if (this._minR === this._circle.r.baseVal.value) { return; }
+
+			do { this._copy.r.baseVal.value = this._copy.r.baseVal.value - 20; }
+			while (!this._isOut(textEl));
+
+			this._copy.r.baseVal.value = this._copy.r.baseVal.value + 20;
+			if (this._circle.r.baseVal.value !== this._copy.r.baseVal.value) {
+				this._circle.r.baseVal.value = this._copy.r.baseVal.value;
+			}
+		}
+	}
+
+	/**
+	 * @param {SVGTextElement} textEl
+	 * @returns {boolean}
+	 * @private
+	 */
+	_isOut(textEl) {
+		return svgTextIsOut(textEl, this._copy, 5);
 	}
 }
