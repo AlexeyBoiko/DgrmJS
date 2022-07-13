2707145495b0479b8f5a476ff862c242c928bdbd
Alexey Boyko <alex@boyko.tech>
2022-07-13 20:23:04 +0300

    remove unused code

--- src/diagram/infrastructure/iterable-utils.js
+++ src/diagram/infrastructure/iterable-utils.js
@@ -46,21 +46,6 @@ export function any(iterable, filter) {
 	return false;
 }
 
-// /**
-//  * @template T
-//  * @template TRes
-//  * @param {Iterable<T>} iterable
-//  * @param {(el:T) => TRes} mapFn
-//  * @returns {Array<TRes>}
-//  */
-// export function map(iterable, mapFn) {
-// 	const res = [];
-// 	for (const el of iterable) {
-// 		res.push(mapFn(el));
-// 	}
-// 	return res;
-// }
-
 /**
  * @template T
  * @param {Set<T>} set
