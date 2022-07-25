44ad16d3edd24d001c8ff53084dc818e660a2644
Alexey Boyko <alex@boyko.tech>
2022-07-25 20:01:55 +0300

    fix connector-end up above shape don't remove hover

--- src/diagram/event-processors/shape-evt-proc.js
+++ src/diagram/event-processors/shape-evt-proc.js
@@ -28,6 +28,7 @@ export class ShapeEvtProc {
 		switch (evt.type) {
 			case 'pointermove':
 				if (!shape[movedDelta]) {
+					console.log('MOVE', shapeOrPath(shape));
 					//
 					// move start
 
@@ -61,19 +62,28 @@ export class ShapeEvtProc {
 				//
 				// move end
 
-				delete shape[movedDelta];
-				disable(shape, false);
+				if (shape.connectable) {
+					if (/** @type {IPresenterConnector} */(evt.detail.target).connectorType === 'in') {
+						//
+						// connect
 
-				if (shape.connectable && /** @type {IPresenterConnector} */(evt.detail.target).connectorType === 'in') {
-					//
-					// connect
+						const path = first(shape.connectedPaths);
+						if (!this._diagram.dispatch('connect', path)) { return; }
+						this._connectorManager.replaceEnd(path, /** @type {IPresenterConnector} */(evt.detail.target));
+						this._diagram.del(shape);
+						shapeStateDel(path, 'disabled');
+						shape = null;
+					}
 
-					const path = first(shape.connectedPaths);
-					if (!this._diagram.dispatch('connect', path)) { return; }
-					this._connectorManager.replaceEnd(path, /** @type {IPresenterConnector} */(evt.detail.target));
-					this._diagram.del(shape);
+					switch (evt.detail.target.type) {
+						case 'shape': shapeStateDel(/** @type {IPresenterShape} */(evt.detail.target), 'hovered'); break;
+						case 'connector': shapeStateDel(/** @type {IPresenterConnector} */(evt.detail.target).shape, 'hovered'); break;
+					}
+				}
 
-					shapeStateDel(/** @type {IPresenterConnector} */(evt.detail.target).shape, 'hovered');
+				if (shape) {
+					disable(shape, false);
+					delete shape[movedDelta];
 				}
 				break;
 
