823cbf8ba118107f20d0f86cdf7a9131198afb89
Alexey Boyko <alex@boyko.tech>
2022-09-12 18:28:25 +0300

    scale - trackpad

--- src/diagram/features/scale-feature.js
+++ src/diagram/features/scale-feature.js
@@ -3,12 +3,15 @@
  * @param {SVGSVGElement} svg
  */
 export function scaleFeature(diagram, svg) {
-	const scaleStep = 0.25;
 	svg.addEventListener('wheel', /** @param {WheelEvent} evt */ evt => {
+		evt.preventDefault();
 		diagram.selected = null;
 
+		const scaleStep = Math.abs(evt.deltaY) < 50
+			? 0.05 // pitch
+			: 0.25; // mouse wheel
 		const nextScale = diagram.scale + (evt.deltaY < 0 ? scaleStep : -scaleStep);
-		if (nextScale < scaleStep || nextScale > 4) { return; }
+		if (nextScale < 0.25 || nextScale > 4) { return; }
 
 		diagram.scaleSet(nextScale, { x: evt.clientX, y: evt.clientY });
 	});
