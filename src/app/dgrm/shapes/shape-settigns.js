fd1a861407d9031c03b6faf4c069f9a1142bffa8
Alexey Boyko <alex@boyko.tech>
2022-10-18 08:24:23 +0300

    shape color - fix Iphone

--- src/app/dgrm/shapes/shape-settigns.js
+++ src/app/dgrm/shapes/shape-settigns.js
@@ -10,9 +10,6 @@ export class ShapeSettings extends HTMLElement {
 				padding: 10px;
 			}
 			[data-cmd] { cursor: pointer; }
-			[data-cmd] * {
-				pointer-events: none;
-			}
 
 			.crcl {
 				width: 25px;
@@ -53,11 +50,11 @@ export class ShapeSettings extends HTMLElement {
 				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M12.9 6.858l4.242 4.243L7.242 21H3v-4.243l9.9-9.9zm1.414-1.414l2.121-2.122a1 1 0 0 1 1.414 0l2.829 2.829a1 1 0 0 1 0 1.414l-2.122 2.121-4.242-4.242z" fill="rgba(52,71,103,1)"/></svg>
 			</div>
 			<div data-cmd="del">
-				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" style="pointer-events: none;"><path fill="none" d="M0 0h24v24H0z"/><path d="M17 6h5v2h-2v13a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V8H2V6h5V3a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v3zm1 2H6v12h12V8zm-9 3h2v6H9v-6zm4 0h2v6h-2v-6zM9 4v2h6V4H9z" fill="rgba(52,71,103,1)"/></svg>
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M17 6h5v2h-2v13a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V8H2V6h5V3a1 1 0 0 1 1-1h8a1 1 0 0 1 1 1v3zm1 2H6v12h12V8zm-9 3h2v6H9v-6zm4 0h2v6h-2v-6zM9 4v2h6V4H9z" fill="rgba(52,71,103,1)"/></svg>
 			</div>
 		</div>`;
 
-		shadow.addEventListener('click', this);
+		shadow.querySelectorAll('[data-cmd]').forEach(el => el.addEventListener('click', this));
 
 		/** @private */
 		this._parent = shadow.host.parentElement;
@@ -69,9 +66,9 @@ export class ShapeSettings extends HTMLElement {
 		this._props = shadow.querySelector('.props');
 	}
 
-	/** @param {PointerEvent & { target: Element }} evt */
+	/** @param {PointerEvent & { currentTarget: Element }} evt */
 	handleEvent(evt) {
-		const cmd = evt.target.getAttribute('data-cmd');
+		const cmd = evt.currentTarget.getAttribute('data-cmd');
 
 		if (cmd === 'toggle') {
 			this._toggle();
@@ -80,7 +77,7 @@ export class ShapeSettings extends HTMLElement {
 
 		this.dispatchEvent(new CustomEvent('cmd', {
 			cancelable: true,
-			detail: { cmd, arg: evt.target.getAttribute('data-cmd-arg') }
+			detail: { cmd, arg: evt.currentTarget.getAttribute('data-cmd-arg') }
 		}));
 	}
 
@@ -88,10 +85,10 @@ export class ShapeSettings extends HTMLElement {
 	_toggle() {
 		if (this._props.style.display === 'none') {
 			this._props.style.display = 'unset';
-			this._parent.style.top = `${this._parent.getBoundingClientRect().top - 88}px`;
+			this._parent.style.top = `${this._parent.getBoundingClientRect().top + window.scrollY - 88}px`; // window.scrollY fix IPhone keyboard
 		} else {
 			this._props.style.display = 'none';
-			this._parent.style.top = `${this._parent.getBoundingClientRect().top + 88}px`;
+			this._parent.style.top = `${this._parent.getBoundingClientRect().top + window.scrollY + 88}px`; // window.scrollY fix IPhone keyboard
 		}
 	}
 }
