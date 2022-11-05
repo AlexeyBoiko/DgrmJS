a6e8e4e55d7becb31aa9de24464fa79c46e0d142
Alexey Boyko <alex@boyko.tech>
2022-11-05 10:32:43 +0300

    grid alignment -  tmp

--- src/diagram-extensions/grid-feature.js
+++ src/diagram-extensions/grid-feature.js
@@ -4,6 +4,8 @@
  */
 export function gridFeature(diagram, svg) {
 	let curOpacity;
+
+	/** @param {number} opacity */
 	function backImg(opacity) {
 		if (curOpacity !== opacity) {
 			curOpacity = opacity;
@@ -12,7 +14,20 @@ export function gridFeature(diagram, svg) {
 	}
 
 	backImg(0.7);
-	svg.style.backgroundSize = '25px 25px';
+
+	const cellSize = 24;
+	const cellSizeHalf = cellSize / 2;
+
+	/**
+	 * @param {number} coordinate
+	 * @return {number}
+	 */
+	function placeToCell(coordinate) {
+		const coor = (Math.round(coordinate / cellSize) * cellSize);
+		return (coordinate - coor > 0) ? coor + cellSizeHalf : coor - cellSizeHalf;
+	}
+
+	svg.style.backgroundSize = `${cellSize}px ${cellSize}px`;
 
 	diagram
 		.on('canvmove', _ => {
@@ -21,13 +36,21 @@ export function gridFeature(diagram, svg) {
 		})
 		.on('scale', _ => {
 			const pos = diagram.canvasPosition;
-			const size = 25 * diagram.scale;
+			const size = cellSize * diagram.scale;
 
 			if (diagram.scale < 0.5) { backImg(0); } else
 			if (diagram.scale <= 0.9) { backImg(0.3); } else { backImg(0.7); }
 
 			svg.style.backgroundSize = `${size}px ${size}px`;
 			svg.style.backgroundPosition = `${pos.x}px ${pos.y}px`;
+		})
+		.on('moveend', /** @param {CustomEvent<IDiagramEventDetail<IDiagramShape>>} evt */ evt => {
+			const pos = evt.detail.target.positionGet();
+			diagram.shapeUpdate(evt.detail.target, {
+				position: {
+					x: placeToCell(pos.x),
+					y: placeToCell(pos.y)
+				}
+			});
 		});
-		// .on('moveend', evt => console.log(evt));
 }
