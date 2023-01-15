c6f296d1f2ff3ce090c4c7dfb5641fc10021690a
Alexey Boyko <alex@boyko.tech>
2023-01-15 11:45:14 +0300

    dgrpm clear path

--- src/diagram/dgrm-clear.js
+++ src/diagram/dgrm-clear.js
@@ -1,13 +1,13 @@
 import { CanvasSmbl } from '../infrastructure/move-scale-applay.js';
+import { PathSmbl } from '../shapes/path.js';
 import { ShapeSmbl } from '../shapes/shape-smbl.js';
 
 /** @param {CanvasElement} canvas */
 export function dgrmClear(canvas) {
 	while (canvas.firstChild) {
-		/** @type {ShapeElement} */(canvas.firstChild)[ShapeSmbl]?.del();
+		(canvas.firstChild[ShapeSmbl] || canvas.firstChild[PathSmbl]).del();
 	}
 	canvas[CanvasSmbl].move(0, 0, 1);
 }
 
-/** @typedef { import("../shapes/shape-evt-proc").ShapeElement } ShapeElement */
 /** @typedef { import('../infrastructure/move-scale-applay.js').CanvasElement } CanvasElement */
